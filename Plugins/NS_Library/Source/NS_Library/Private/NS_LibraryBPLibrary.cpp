36767328f25333d94434c928d47f81a7047e7839
Nino Saglia <nino.saglia97@gmail.com>
2022-10-06 01:16:28 +0200

    Working Prototype using a datamap as input

---
+++ Plugins/NS_Library/Source/NS_Library/Private/NS_LibraryBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+#include "AudioSynesthesiaNRTFactory.h"
+#include "NS_LibraryBPLibrary.h"
+#include "NS_Library.h"
+
+UNS_LibraryBPLibrary::UNS_LibraryBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+int UNS_LibraryBPLibrary::NS_LibrarySampleFunction(int Paramam)
+{
+	return -10;
+}
+
---
+++ Source/Hit_Reaction_Sound_Tests/Hit_Reaction_Sound_Tests.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Hit_Reaction_Sound_Tests.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Hit_Reaction_Sound_Tests, "Hit_Reaction_Sound_Tests" );
